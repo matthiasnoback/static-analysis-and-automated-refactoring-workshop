ac268c121c81958a631ba9249f77ce6e626f92d5
Matthias Noback <matthiasnoback@gmail.com>
2021-12-08 11:56:40 +0100

    Module6

--- src/Module6/okay.php
+++ src/Module6/okay.php
@@ -4,4 +4,12 @@ declare(strict_types=1);
 
 namespace App\Module6;
 
+use Assert\Assertion;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
 $event = new Event(\DateTimeImmutable::createFromFormat('Y-m-d', '1970-01-01'));
+
+$date = \DateTimeImmutable::createFromFormat('Y-m-d', $_GET['date']);
+Assertion::isInstanceOf($date, \DateTimeImmutable::class);
+$event = new Event($date);
