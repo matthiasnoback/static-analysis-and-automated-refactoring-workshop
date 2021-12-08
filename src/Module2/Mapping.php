06159e26b9ffa4796985130da3c1def82f84045e
Matthias Noback <matthiasnoback@gmail.com>
2021-12-08 11:10:54 +0100

    Module2

---
+++ src/Module2/Mapping.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Module2;
+
+trait Mapping
+{
+    /**
+     * @param array<mixed> $data
+     */
+    private static function getString(array $data, string $key): string
+    {
+        if (!isset($data[$key])) {
+            throw new \InvalidArgumentException();
+        }
+
+        $value = $data[$key];
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException();
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array<mixed> $data
+     */
+    private static function getInt(array $data, string $key): int
+    {
+        if (!isset($data[$key])) {
+            throw new \InvalidArgumentException();
+        }
+
+        $value = $data[$key];
+        if (is_string($value)) {
+            return (int) $value;
+        }
+
+        if (is_int($value)) {
+            return $value;
+        }
+
+        throw new \InvalidArgumentException();
+    }
+}
