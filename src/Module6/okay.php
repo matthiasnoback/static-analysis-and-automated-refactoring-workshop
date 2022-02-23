0981356a5ad6f9965adaa32292eb52b14a178f7d
Matthias Noback <matthiasnoback@gmail.com>
2022-02-23 11:56:10 +0100

    Module 6

--- src/Module6/okay.php
+++ src/Module6/okay.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 
 namespace App\Module6;
 
-$event = new Event(\DateTimeImmutable::createFromFormat('Y-m-d', '1970-01-01'));
+use Assert\Assertion;
+
+require 'vendor/autoload.php';
+
+$happenedAt = \DateTimeImmutable::createFromFormat('Y-m-d', '1970-01-01');
+$event = new Event($happenedAt);
