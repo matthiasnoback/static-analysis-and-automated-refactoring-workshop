8fbee6826a3948cd519da70cc68c044a458de4e9
Matthias Noback <matthiasnoback@gmail.com>
2022-02-24 09:30:59 +0100

    Module 9

--- utils/PHPStan/src/DynamicInstantiationRule.php
+++ utils/PHPStan/src/DynamicInstantiationRule.php
@@ -4,22 +4,35 @@ declare(strict_types=1);
 namespace Utils\PHPStan;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\Variable;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
 
+/**
+ * @implements Rule<New_>
+ */
 final class DynamicInstantiationRule implements Rule
 {
     public function getNodeType(): string
     {
-        // What node type is this rule interested in? For now: any node type
-        return Node::class;
+        // What node type is this rule interested in?
+        return New_::class;
     }
 
+    /**
+     * @param New_ $node
+     */
     public function processNode(Node $node, Scope $scope): array
     {
-        // Return errors for this node. For now: nothing, just print the type of the node
-        echo get_class($node) . "\n";
+        if (!$node->class instanceof Variable) {
+            return [];
+        }
 
-        return [];
+        return [
+            RuleErrorBuilder::message('Dynamic class instantiation is not allowed')
+                ->build()
+        ];
     }
 }
---
+++ utils/PHPStan/tests/DynamicInstantiationRule/Fixtures/temp.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+new DateTimeImmutable('now');
+new $var('now');
+new class() {};
