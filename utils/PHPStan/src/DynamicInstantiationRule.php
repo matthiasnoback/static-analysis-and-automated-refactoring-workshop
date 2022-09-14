cc667d1c3e7a6a9efc148a87d7c5d9324c4454dd
Matthias Noback <matthiasnoback@gmail.com>
2022-09-14 14:15:51 +0200

    Module 9

--- utils/PHPStan/src/DynamicInstantiationRule.php
+++ utils/PHPStan/src/DynamicInstantiationRule.php
@@ -5,22 +5,35 @@ declare(strict_types=1);
 namespace Utils\PHPStan;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Name\FullyQualified;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
 
+/**
+ * @implements Rule<New_>
+ */
 final class DynamicInstantiationRule implements Rule
 {
     public function getNodeType(): string
     {
-        // What node type is this rule interested in? For now: any node type
-        return Node::class;
+        return New_::class;
     }
 
+    /**
+     * @param New_ $node
+     */
     public function processNode(Node $node, Scope $scope): array
     {
-        // Return errors for this node. For now: nothing, just print the type of the node
-        echo get_class($node) . "\n";
+        if ($node->class instanceof FullyQualified) {
+            return [];
+        }
 
-        return [];
+        return [
+            RuleErrorBuilder::message(
+                'Dynamic class instantiation is not allowed'
+            )->build()
+        ];
     }
 }
--- utils/PHPStan/tests/DynamicInstantiationRule/DynamicInstantiationRuleTest.php
+++ utils/PHPStan/tests/DynamicInstantiationRule/DynamicInstantiationRuleTest.php
@@ -8,12 +8,13 @@ use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use Utils\PHPStan\DynamicInstantiationRule;
 
+/**
+ * @extends RuleTestCase<DynamicInstantiationRule>
+ */
 final class DynamicInstantiationRuleTest extends RuleTestCase
 {
     public function testRulePreventsDynamicInstantiation(): void
     {
-        $this->markTestIncomplete('Enable this test when working on Module9');
-
         $this->analyse(
             [__DIR__ . '/Fixtures/dynamic-instantiation.php'],
             [['Dynamic class instantiation is not allowed', 3]]
@@ -22,8 +23,6 @@ final class DynamicInstantiationRuleTest extends RuleTestCase
 
     public function testRuleSkipsNormalClassNameInstantiation(): void
     {
-        $this->markTestIncomplete('Enable this test when working on Module9');
-
         $this->analyse(
             [__DIR__ . '/Fixtures/skip-class-name-instantiation.php'],
             [] // no errors
