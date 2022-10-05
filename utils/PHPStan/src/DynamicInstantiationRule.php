7beff07f4d52307d3ae862167f5f7a1985ff48ba
Matthias Noback <matthiasnoback@gmail.com>
2022-10-05 14:36:09 +0200

    Module 9

--- utils/PHPStan/src/DynamicInstantiationRule.php
+++ utils/PHPStan/src/DynamicInstantiationRule.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Utils\PHPStan;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\New_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 
@@ -12,15 +14,21 @@ final class DynamicInstantiationRule implements Rule
 {
     public function getNodeType(): string
     {
-        // What node type is this rule interested in? For now: any node type
-        return Node::class;
+        return New_::class;
     }
 
+    /**
+     * @param New_ $node
+     */
     public function processNode(Node $node, Scope $scope): array
     {
-        // Return errors for this node. For now: nothing, just print the type of the node
-        echo get_class($node) . "\n";
+        if ($node->class instanceof Expr) {
+            return [
+                'Dynamic class instantiation is not allowed'
+            ];
+        }
 
+        // Everything is okay
         return [];
     }
 }
--- utils/PHPStan/tests/DynamicInstantiationRule/DynamicInstantiationRuleTest.php
+++ utils/PHPStan/tests/DynamicInstantiationRule/DynamicInstantiationRuleTest.php
@@ -8,12 +8,13 @@ use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use Utils\PHPStan\DynamicInstantiationRule;
 
+/**
+ * @group wip
+ */
 final class DynamicInstantiationRuleTest extends RuleTestCase
 {
     public function testRulePreventsDynamicInstantiation(): void
     {
-        $this->markTestIncomplete('Enable this test when working on Module9');
-
         $this->analyse(
             [__DIR__ . '/Fixtures/dynamic-instantiation.php'],
             [['Dynamic class instantiation is not allowed', 3]]
@@ -22,10 +23,16 @@ final class DynamicInstantiationRuleTest extends RuleTestCase
 
     public function testRuleSkipsNormalClassNameInstantiation(): void
     {
-        $this->markTestIncomplete('Enable this test when working on Module9');
-
         $this->analyse(
             [__DIR__ . '/Fixtures/skip-class-name-instantiation.php'],
+            [] // no errors
+        );
+    }
+
+    public function testRuleSkipsInlineClassDefinitions(): void
+    {
+        $this->analyse(
+            [__DIR__ . '/Fixtures/skip-inline-class.php'],
             [] // no errors
         );
     }
